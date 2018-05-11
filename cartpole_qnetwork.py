a2550c670ec62313b51258ee567195c051dfe43f
Emanuel Geromin <egeromin@gmail.com>
2018-05-11 10:32:46 -0400

    Current updates to cartpole_qnetwork.
    Not working yet!

--- cartpole_qnetwork.py
+++ cartpole_qnetwork.py
@@ -9,22 +9,28 @@ from keras.layers import Dense
 
 class QNetwork:
 
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, next_state_dim, action_dim):
         self.q = Sequential()
         self.q.add(Dense(units=state_dim*3, activation='relu', input_dim=state_dim))
         self.q.add(Dense(units=state_dim*3, activation='relu'))
         self.q.add(Dense(units=state_dim*3, activation='relu'))
-        self.q.add(Dense(units=action_dim, activation='softmax'))
+        self.q.add(Dense(units=action_dim, activation=None))
         self.q.compile(loss='mean_squared_error',
             optimizer='adam',
             metrics=['accuracy'])
 
+    def predict(self, state):
+        return self.q.predict(state)
+
+    def update(self, target, curr_state):
+        return self.q.fit(curr_state, target)
 
 class QCartPoleSolver():
     def __init__(self):
         self.env = gym.make('CartPole-v1')
+        self.discount_rate = 0.99
 
-        state_dim = self.env.observation_space.shape[0] # Number of dims per layer
+        next_state_dim = self.env.observation_space.shape[0] # Number of dims per layer
         action_dim = self.env.action_space.n
         self.q_network = QNetwork(state_dim, action_dim)
 
@@ -35,18 +41,18 @@ class QCartPoleSolver():
         return max(0.1, min(1.0, 1.0 - math.log10((t + 1) / 25)))
 
     def choose_action(self, state, explore_rate):
-        return self.env.action_space.sample() if (np.random.random() < explore_rate) else 0
+        return self.env.action_space.sample() if (np.random.random() < explore_rate) else np.argmax(self.q_network.predict(state))
 
-    # def update_q(self, state_0, action, reward, state, learning_rate):
-        # self.q[state_0][action] += learning_rate*(reward + 0.99 * np.max(self.q[state]) - self.q[state_0][action])
-
-    def update_network(self):
-        pass
+    def update_network(self, curr_state, action, reward, next_state):
+        new_q = reward + self.discount_rate * np.max(self.q_network.predict(next_state))
+        target = self.q_network.predict(curr_state)
+        target[action] = new_q
+        self.q_network.update(target, curr_state)
 
     def run(self):
         rewards = []
         for i in range(1000):
-            state_0 = self.env.reset()
+            curr_state = self.env.reset()
 
             learning_rate = self.get_learning_rate(i)
             explore_rate = self.get_explore_rate(i)
@@ -54,14 +60,13 @@ class QCartPoleSolver():
 
             for j in range(500):
                 # self.env.render()
-                action = self.choose_action(state_0, explore_rate)
+                action = self.choose_action(curr_state, explore_rate)
                 obs, reward, done, _ = self.env.step(action)
 
-                state = obs
-                # self.update_q(state_0, action, reward, state, learning_rate)
-                self.update_network()
+                next_state = obs
+                self.update_network(curr_state, action, reward, next_state)
 
-                state_0 = state
+                curr_state = next_state
                 episode_rewards += reward
 
                 if done:
